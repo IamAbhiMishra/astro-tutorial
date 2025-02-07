6959624eb9222889064c256cb083aa3b99fb0f64
IamAbhiMishra <abhishekmishra1041@gmail.com>
2025-02-07 12:51:26 +0530

    updated rss, added tag index, post layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,6 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: "https://spectacular-pegasus-e308d2.netlify.app/"
+});
